bde3f9071bfae800c81628951fd7d934a4ab0bb4
Ming-Yuan Lu <michaelmylu@gmail.com>
2019-06-12 12:09:47 -0400

    Debugged themes split

--- src/spark/get-popular-topics.py
+++ src/spark/get-popular-topics.py
@@ -53,7 +53,8 @@ gkgRDD = gkgRDD.map(lambda x: x.split('\t'))
 gkgRowRDD = gkgRDD.map(lambda x : Row(src_common_name = x[3],
                                     doc_id = x[4],
                                     #themes = map(lambda s: s.split(';')[:-1], x[7])
-                                    themes = x[7].map(lambda x: x.split(';')[:-1])
+                                    #themes = x[7].map(lambda x: x.split(';')[:-1])
+                                    themes = x[7].split(';')[:-1]
                                     ))
 
 
