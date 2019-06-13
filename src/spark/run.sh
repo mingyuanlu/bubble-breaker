f663bc283b7df089d7a6717d0a4851725d204798
Ming-Yuan Lu <michaelmylu@gmail.com>
2019-06-13 02:04:07 -0400

    Changed spark.cassandra.connection.host to the IP of the cassandra cluster

--- src/spark/run.sh
+++ src/spark/run.sh
@@ -3,7 +3,7 @@ spark-submit \
   --executor-memory 1G \
   --driver-memory 8G \
   --packages anguenot/pyspark-cassandra:0.9.0,com.databricks:spark-csv_2.10:1.2.0 \
-  --conf spark.cassandra.connection.host=ip1.***.***.**,ip2.***.***.**,ip3.***.***.** \
+  --conf spark.cassandra.connection.host=172.31.90.30 \
   --conf spark.driver.port=51810 \
   --conf spark.fileserver.port=51811 \
   --conf spark.broadcast.port=51812 \
