0dd4d2b9f031c39ac9c5377500e0005ce27f8afb
Ming-Yuan Lu <michaelmylu@gmail.com>
2019-06-18 23:15:09 -0400

    Update spark-submit options

--- src/spark/run.sh
+++ src/spark/run.sh
@@ -1,13 +1,7 @@
 # !/bin/bash
 spark-submit \
   --executor-memory 1G \
-  --driver-memory 8G \
-  --packages anguenot/pyspark-cassandra:0.9.0,com.databricks:spark-csv_2.10:1.2.0 \
-  --conf spark.cassandra.connection.host=172.31.90.30 \
-  --conf spark.driver.port=51810 \
-  --conf spark.fileserver.port=51811 \
-  --conf spark.broadcast.port=51812 \
-  --conf spark.replClassServer.port=51813 \
-  --conf spark.blockManager.port=51814 \
-  --conf spark.executor.port=51815 \
+  --driver-memory 6G \
+  --master spark://ip-10-0-0-8:7077 \
+  --num-executors 3
   compute-avg-tone-per-theme.py
