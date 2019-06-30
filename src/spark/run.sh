15df954a32bc98a06271fb668d5e82badd903ac7
Ming-Yuan Lu <michaelmylu@gmail.com>
2019-06-30 16:48:37 +0000

    Added new files that builds the theme taxonomy dictionary, obtains the top 1000 popular themes, and compute the analytics of themse according to these files.

--- src/spark/run.sh
+++ src/spark/run.sh
@@ -1,7 +1,10 @@
 # !/bin/bash
 spark-submit \
-  --executor-memory 1G \
+  --executor-memory 5G \
   --driver-memory 6G \
   --master spark://ip-10-0-0-8:7077 \
-  --num-executors 3
-  compute-avg-tone-per-theme.py
+  --num-executors 3 \
+ --conf spark.executor.memoryOverhead=600 \
+  --total-executor-cores 6 \
+  --py-files functions.py \
+  $1
