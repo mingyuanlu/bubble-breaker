b6929f80d31042d6bf3dd81f566d6e2d870348a7
Ming-Yuan Lu <michaelmylu@gmail.com>
2019-07-07 05:42:12 +0000

    Modifed config to reflect cluster resources

--- src/spark/run.sh
+++ src/spark/run.sh
@@ -1,10 +1,13 @@
 # !/bin/bash
 spark-submit \
-  --executor-memory 5G \
+  --executor-memory 3G \
   --driver-memory 6G \
   --master spark://ip-10-0-0-13:7077 \
-  --num-executors 4 \
- --conf spark.executor.memoryOverhead=600 \
-  --total-executor-cores 8 \
+  --num-executors 7 \
+  --executor-cores 5 \
+  --conf spark.executor.memoryOverhead=600 \
+  --conf "spark.executor.extraJavaOptions=-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps" \
+  --conf "spark.driver.extraJavaOptions=-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps" \
   --py-files functions.py \
+  --total-executor-cores 35 \
   $1
