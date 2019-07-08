fe60eb59ab87c7a0759522c36b8fcf5dba56db59
Ming-Yuan Lu <michaelmylu@gmail.com>
2019-07-08 05:44:45 +0000

    Modified spark-submit options for stable jobs

--- src/spark/run.sh
+++ src/spark/run.sh
@@ -1,13 +1,13 @@
-# !/bin/bash
+#!/bin/bash
 spark-submit \
-  --executor-memory 3G \
-  --driver-memory 6G \
-  --master spark://ip-10-0-0-13:7077 \
-  --num-executors 7 \
-  --executor-cores 5 \
-  --conf spark.executor.memoryOverhead=600 \
-  --conf "spark.executor.extraJavaOptions=-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps" \
-  --conf "spark.driver.extraJavaOptions=-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps" \
-  --py-files functions.py \
-  --total-executor-cores 35 \
-  $1
+   --master spark://ip-10-0-0-13:7077 \
+   --executor-memory 6G \
+   --driver-memory 6G \
+   --num-executors 4 \
+   --executor-cores 1 \
+   --conf spark.executor.memoryOverhead=1024 \
+   --conf "spark.executor.extraJavaOptions=-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps" \
+   --conf "spark.driver.extraJavaOptions=-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps" \
+   --py-files functions.py \
+   --total-executor-cores 4 \
+   $1
