30ebefa0e231be7a91955ad940452553e0adbfef
Ming-Yuan Lu <michaelmylu@gmail.com>
2019-07-08 06:44:13 +0000

    Added master script to run bubble-breaker batch job

---
+++ src/spark/run-generate-list.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+spark-submit \
+   --master spark://ip-10-0-0-13:7077 \
+   --executor-memory 6G \
+   --driver-memory 6G \
+   --num-executors 4 \
+   --executor-cores 1 \
+   --conf spark.executor.memoryOverhead=1024 \
+   --conf "spark.executor.extraJavaOptions=-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps" \
+   --conf "spark.driver.extraJavaOptions=-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps" \
+   --py-files functions.py \
+   --total-executor-cores 4 \
+   $1 $2
